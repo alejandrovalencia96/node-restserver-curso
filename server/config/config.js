83e6acc56c7192edf9349ff9b59697d4f2d9f3a8
Alejandro Valencia <alejandrovalencia012@hotmail.com>
2020-08-10 09:55:46 -0500

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -2,3 +2,23 @@
 // PUERTO 
 //==============
 process.env.PORT = process.env.PORT || 3000;
+
+
+//==============
+// Entorno 
+//==============
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+//==============
+// Base de datos 
+//==============
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb+srv://strider:FVZWlt9uQqe8Svaz@cluster0.oh894.mongodb.net/cafe';
+}
+
+process.env.URLDB = urlDB;
